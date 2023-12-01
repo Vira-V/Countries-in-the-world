836e4aaa0a29f34af2bf05be7a2acbdff78fb360
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2023-12-01 20:09:22 +0100

    added Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { CardList } from './components/CardList/CardList';
+import { Header } from './components/Header';
 
 export const App: React.FC = () => {
   const [country, setCountry] = useState([]);
   const [loading, setLoading] = useState(false);
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const data = await fetch('https://restcountries.com/v2/all').then(response => response.json());
+        const data = await fetch('https://restcountries.com/v2/all').then(
+          (response) => response.json(),
+        );
         setCountry(data);
         setLoading(false);
       } catch {
@@ -20,6 +23,10 @@ export const App: React.FC = () => {
     fetchData();
   }, []);
 
-  return <CardList countries={country} loading={loading}/>;
+  return (
+    <>
+      <Header />
+      <CardList countries={country} loading={loading} />
+    </>
+  );
 };
-
