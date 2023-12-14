e03056c7263ac59da4d185700d28b762fde9836a
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2023-12-14 19:37:20 +0100

    lazy loading

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './CardList.scss';
 import { Country } from '../types/Country';
 import { Card } from '../Card/Card';
@@ -9,10 +9,59 @@ interface ListProps {
   setDetails: (details: Country) => void;
 }
 
-export const CardList: React.FC<ListProps> = ({ countries, loading, setDetails }) => (
-  <div className="countries-container">
-    {loading || countries.map((country) => (
-      <Card key={country.numericCode} country={country} setDetails={setDetails}/>
-    ))}
-  </div>
-);
+export const CardList: React.FC<ListProps> = ({
+  countries,
+  loading,
+  setDetails,
+}) => {
+  const [visibleCountries, setVisibleCountries] = useState<Country[]>([]);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    updateVisibleCountries();
+  }, [countries]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            updateVisibleCountries();
+          }
+        });
+      },
+      { threshold: 0.5 },
+    );
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, [visibleCountries]);
+
+  const updateVisibleCountries = () => {
+    const cardHeight = 320;
+    const visibleCount = Math.ceil(window.innerHeight / cardHeight);
+
+    const newVisibleCountries = countries.slice(0, visibleCount);
+
+    setVisibleCountries((prevVisible) => [
+      ...prevVisible,
+      ...newVisibleCountries,
+    ]);
+  };
+
+  return (
+    <div className="countries-container">
+      {loading ||
+        countries.map((country) => (
+          <Card
+            key={country.numericCode}
+            country={country}
+            setDetails={setDetails}
+          />
+        ))}
+    </div>
+  );
+};
